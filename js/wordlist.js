2a3200e5083e684825ae1ad58d8bd0d4893d0d81
Christian Schiller <cschiller@users.noreply.github.com>
2019-08-09 19:10:59 +0800

    Improved word list

--- js/wordlist.js
+++ js/wordlist.js
@@ -4,18 +4,47 @@
  https://chrome.google.com/extensions/detail/kkmlkkjojmombglmlpbpapmhcaljjkde
  */
 
+const NOTES_COLUMN = 4;
+
 let wordList = localStorage['wordlist'];
 
 let entries;
 if (wordList) {
     entries = JSON.parse(wordList);
+    entries.forEach(e => { e.notes = (e.notes || '<i>Edit</i>');});
 } else {
     entries = [];
 }
 
+function showListIsEmptyNotice() {
+    if (entries.length === 0) {
+        $('#nodata').show();
+    } else {
+        $('#nodata').hide();
+    }
+}
+
+function disableButtons() {
+    if (entries.length === 0) {
+        $('#saveList').prop('disabled', true);
+        $('#selectAll').prop('disabled', true);
+        $('#deselectAll').prop('disabled', true);
+        $('#delete').prop('disabled', true);
+    } else {
+        $('#saveList').prop('disabled', false);
+        $('#selectAll').prop('disabled', false);
+        $('#deselectAll').prop('disabled', false);
+        $('#delete').prop('disabled', false);
+    }
+}
+
 $(document).ready(function () {
 
+    showListIsEmptyNotice();
+    disableButtons();
+
     let wordsElement = $('#words');
+    let invalidateRow;
     let table = wordsElement.DataTable({
         data: entries,
         columns: [
@@ -23,17 +52,45 @@ $(document).ready(function () {
             { data: 'traditional' },
             { data: 'pinyin' },
             { data: 'definition' },
+            { data: 'notes' },
         ]
     });
 
-    wordsElement.find('tbody').on('click', 'tr', function () {
-        $(this).toggleClass('bg-info');
+    wordsElement.find('tbody').on('click', 'tr', function (event) {
+        if (!event.target._DT_CellIndex || event.target._DT_CellIndex.column === NOTES_COLUMN) {
+            let index = event.currentTarget._DT_RowIndex;
+            let entry = entries[index];
+
+            $('#simplified').val(entry.simplified);
+            $('#traditional').val(entry.traditional);
+            $('#definition').val(entry.definition);
+            $('#notes').val(entry.notes === '<i>Edit</i>' ? '' : entry.notes);
+            $('#rowIndex').val(index);
+
+            $('#editNotes').modal('show');
+            $('#notes').focus();
+
+            invalidateRow = table.row(this).invalidate;
+
+        } else {
+            $(this).toggleClass('bg-info');
+        }
     });
 
-    $('#savebutton').click(function () {
+    $('#editNotes').on('shown.bs.modal', () => $('#notes').focus());
+
+    $('#saveNotes').click(() => {
+        let entry = entries[$('#rowIndex').val()];
+        entry.notes = $('#notes').val() || '<i>Edit</i>';
+        $('#editNotes').modal('hide');
+        invalidateRow().draw();
+        localStorage['wordlist'] = JSON.stringify(entries);
+    });
+
+    $('#saveList').click(function () {
         let selected = table.rows('.bg-info').data();
 
-        if (!selected) {
+        if (selected.length === 0) {
             return;
         }
 
@@ -47,6 +104,8 @@ $(document).ready(function () {
             content += entry.pinyin;
             content += '\t';
             content += entry.definition;
+            content += '\t';
+            content += entry.notes.replace('<i>Edit</i>', '').replace(/[\r\n]/gm, ' ');
             content += '\r\n';
         }
 
@@ -57,11 +116,10 @@ $(document).ready(function () {
         a.click();
     });
 
-    let deleteButton = $('#delete');
-    deleteButton.click(function () {
+    $('#delete').click(function () {
         table.rows('.bg-info').remove();
 
-        let entries = table.rows().data().draw(true);
+        entries = table.rows().data().draw(true);
 
         let toKeep = [];
         for (let i = 0; i < entries.length; i++) {
@@ -70,24 +128,15 @@ $(document).ready(function () {
 
         localStorage['wordlist'] = JSON.stringify(toKeep);
 
-        // location.reload(true);
+        showListIsEmptyNotice();
+        disableButtons();
     });
 
-    $('#selectall').click(function () {
+    $('#selectAll').click(function () {
         $('#words').find('tbody tr').addClass('bg-info');
     });
 
-    $('#deselectall').click(function () {
+    $('#deselectAll').click(function () {
         $('#words').find('tbody tr').removeClass('bg-info');
     });
-
-    if (entries.length > 0) {
-        $('#nodata').hide();
-        $('#save').show();
-        deleteButton.show();
-    } else {
-        $('#nodata').show();
-        $('#save').hide();
-        deleteButton.hide();
-    }
 });
